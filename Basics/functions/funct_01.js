a990051df28e08b88da813e747217b6d1f73d064
23Animesh <duttaanimesh2004@gmail.com>
2023-09-11 21:23:00 +0530

    functions

---
+++ Basics/functions/funct_01.js
@@ -0,0 +1,18 @@
+ // function is basicly a collection of codes
+
+function hello(){                               //creation of functions,declaring
+    console.log("welcome to the function");
+    console.log("new user detected");
+}
+
+//hello()                               //calling of the fubctions
+function sum(number1,number2){          //declaration with parameters
+     console.log(number1+number2);
+}
+
+
+function newFunction(number1,number2) {
+    //let result = number1+ number2
+    return number1+ number2
+}
+const result = sum(12,16)                           //calling with arguements 
